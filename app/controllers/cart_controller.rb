18a727f9fb7d622ada46785606a0efe7c2c3408d
luis henrique <luisham2001@gmail.com>
2026-03-23 11:08:28 -0300

    bazar 1.0_fixes

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -6,8 +6,11 @@ class CartController < ApplicationController
 
   def add
     session[:cart] ||= {}
+    quantity = params[:quantity].to_i
+    return redirect_to products_path, alert: "Quantidade deve ser maior que zero!" if quantity <= 0
+
     session[:cart][params[:variant_id]] ||= 0
-    session[:cart][params[:variant_id]] += 1
+    session[:cart][params[:variant_id]] += quantity
     if session[:cart][params[:variant_id]] > ProductVariant.find(params[:variant_id]).stock
       session[:cart][params[:variant_id]] = ProductVariant.find(params[:variant_id]).stock
       redirect_to cart_path, alert: "Quantidade máxima disponível atingida!"
